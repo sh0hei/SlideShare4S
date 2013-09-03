513e7b234631f5d144bae15d428282ee6a349d04
Taro Nagasawa <taro.nagasawa@gmail.com>
2013-09-03 14:48:25 +0900

    fixed #2

---
+++ src/test/scala/com/taroid/slideshare4s/XmlToSlideshowsImplTest.scala
@@ -0,0 +1,58 @@
+package com.taroid.slideshare4s
+
+import org.specs2.mutable._
+import org.specs2.mock._
+import java.text.{SimpleDateFormat, DateFormat}
+import scala.xml.XML
+import java.util.Date
+
+class XmlToSlideshowsImplTest extends Specification with Mockito {
+
+  private val xml = XML.loadString("""
+                                     |<Slideshows>
+                                     | <Slideshow>
+                                     |  <ID>111111</ID>
+                                     |  <Title>aaa</Title>
+                                     |  <Username>bbb</Username>
+                                     |  <Description>ccc</Description>
+                                     |  <URL>ddd</URL>
+                                     |  <ThumbnailURL>eee</ThumbnailURL>
+                                     |  <Created>Thu Jan 01 00:00:00 +0900 2013</Created>
+                                     |  <Updated>Thu Jan 01 01:00:00 +0900 2013</Updated>
+                                     |  <Language>ff</Language>
+                                     | </Slideshow>
+                                     |</Slideshows>
+                                   """.stripMargin)
+
+  "convert" should {
+    "引数にnullを渡すと例外を投げる" in {
+      {new XmlToSlideshowsImpl(mock[DateFormat]).convert(null)} must throwA[IllegalArgumentException]
+    }
+
+    "引数にスライド情報を渡すとスライド1つにつきDateFormat#parseを2回呼び出す" in {
+      val dateFormat = mock[DateFormat]
+      dateFormat.parse(anyString) returns new Date(0)
+
+      new XmlToSlideshowsImpl(dateFormat).convert(xml)
+
+      there was two(dateFormat).parse(anyString)
+    }
+
+    "引数にスライド情報を渡すとSlideshowのリストを返す" in {
+      val dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy")
+      val slideshows = new XmlToSlideshowsImpl(dateFormat).convert(xml)
+
+      slideshows must haveTheSameElementsAs(List(Slideshow(
+        id = 111111,
+        title = "aaa",
+        username = "bbb",
+        description = "ccc",
+        url = "ddd",
+        thumbnailUrl = "eee",
+        created = dateFormat.parse("Thu Jan 01 00:00:00 +0900 2013"),
+        updated = dateFormat.parse("Thu Jan 01 01:00:00 +0900 2013"),
+        language = "ff"
+      )))
+    }
+  }
+}
